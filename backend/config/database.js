b5f9e43cb028cd55483284b3d0e260d554d4771f
Anwar Hossain <mahedianwar.com>
2022-12-12 15:48:23 +0600

    backend completd

---
+++ backend/config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const connectDatabase = () => {
+  console.log(process.env.MONGO_URL);
+  mongoose.connect(process.env.MONGO_URL).then((data) => {
+    console.log(`Mongodb connected with server: ${data.connection.host}`);
+  });
+};
+
+module.exports = connectDatabase;
