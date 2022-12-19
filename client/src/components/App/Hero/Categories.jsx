f69916b873baa15722822f28cae30695cb7e7914
Anwar Hossain <mahedianwar@gmail.com>
2022-12-19 10:58:49 +0600

    update category link

--- client/src/components/App/Hero/Categories.jsx
+++ client/src/components/App/Hero/Categories.jsx
@@ -1,12 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import { categories } from '../../../constants/categories';
 
 const Categories = () => {
+  const navigate = useNavigate();
   return (
     <>
       <div className="category">
         {categories.map((cat, index) => {
           return (
-            <div className="box f_flex" key={index}>
+            <div
+              className="box f_flex"
+              key={index}
+              onClick={() => navigate(`products?category=${cat.cateName}`)}
+            >
               <img src={cat.cateImg} alt="" />
               <span>{cat.cateName}</span>
             </div>
