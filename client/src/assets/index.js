245fdc947eb5241d95a3d100f4b366fe3737e897
Restyled.io <commits@restyled.io>
2022-12-23 03:07:28 +0000

    Restyled by clang-format

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -31,9 +31,9 @@ import slide3 from './images/SlideCard/slide-3.png';
 import slide4 from './images/SlideCard/slide-4.png';
 
 const assets = {
-  images: {
+  images : {
     logo,
-    catImage: {
+    catImage : {
       cat1,
       cat2,
       cat3,
@@ -46,20 +46,20 @@ const assets = {
       cat10,
       cat11,
     },
-    slideImage: {
+    slideImage : {
       slide1,
       slide2,
       slide3,
       slide4,
     },
-    flash: {
+    flash : {
       flash1,
       flash2,
       flash3,
       flash4,
       flash5,
     },
-    shops: {
+    shops : {
       shop1,
       shop2,
       shop3,
